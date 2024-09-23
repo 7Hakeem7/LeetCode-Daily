d43728596cab9479f0e850ccd71c9c6112916f4d
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-09-23 21:14:00 +0530

    [1482] [Time Beats: 71.63%] [Memory Beats: 67.73%] - LeetPush

--- Binary Search/1482-Minimum-Number-of-Days-to-Make-m-Bouquets.java
+++ Binary Search/1482-Minimum-Number-of-Days-to-Make-m-Bouquets.java
@@ -1,44 +1,49 @@
-public class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        int low  = Arrays.stream(bloomDay).min().getAsInt();
-        int high =  Arrays.stream(bloomDay).max().getAsInt();
-
-        int  ans = -1;
-
-        while (low<=high){
-            int mid = low+(high-low)/2;
-
-            if(isPossible(bloomDay,mid,k,m)){
-                ans=mid;
-                high=mid-1;
-            }
-            else {
-
-                low=mid+1;
-            }
+class Solution {
+    private boolean isPossible(int day , int[] nums , int k , int m){
+      int count = 0;
+      int noOfBouqs = 0;
+      int n = nums.length;
+      for (int i = 0 ; i<n ; i++){
+        if(nums[i] <= day){
+          count++;
+          if(count == k){
+            noOfBouqs++;
+            count = 0;
+          }
         }
-
-        return ans;
+        else {
+          count = 0;
+        }
+      }
+      return noOfBouqs >= m ? true : false ;
     }
 
-    private boolean isPossible(int[] bloomDay, int mid, int k,int booket) {
-        int bouquets = 0, flowersCollected = 0;
-        for (int value : bloomDay) {
-            if (value <= mid) {
-//                If the current flower can be taken with in days then increase the flower flowersCollected.
-                flowersCollected++;
-            } else {
-//                If there is a flower in between that takes more number of days then the given day, then resent the counter.
-                flowersCollected = 0;
-            }
-//            If the flowersCollected is same as the required flower per bookie, then increase the bouquets count;
-            if (flowersCollected == k) {
-                bouquets++;
-                flowersCollected = 0;
-            }
+    public int minDays(int[] nums, int m, int k) {
+
+        int n = nums.length;
+        
+        if((long)m*k > n) return -1;
+        int ans = -1;
+
+        int mini = Integer.MAX_VALUE , maxi = Integer.MIN_VALUE;
+        for (int i = 0 ; i< n; i++){
+            mini = Math.min(mini , nums[i]);
+            maxi = Math.max(maxi , nums[i]);
         }
 
-        return bouquets>=booket;
+        int low = mini , high = maxi;
 
+        while(low <= high){
+            int day = (low + high)/2;
+
+            if(isPossible(day , nums , k , m)){
+                ans = day;
+                high = day -1;
+            }
+            else{
+                low = day + 1;
+            }
+        }
+        return ans;
     }
 }
