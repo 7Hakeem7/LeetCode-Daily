4b8cd82cd38a6ae47a19ece04e65d6be00eb1c4c
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-10-03 14:23:39 +0530

    [1590] [Time Beats: 66.67%] [Memory Beats: 28.17%] - LeetPush

--- Oct-POTD/1590-Make-Sum-Divisible-by-P.java
+++ Oct-POTD/1590-Make-Sum-Divisible-by-P.java
@@ -1,28 +1,24 @@
-import java.util.HashMap;
-
-public class Solution {
+class Solution {
     public int minSubarray(int[] nums, int p) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum = (totalSum + num) % p;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int ans = nums.length;
+        int sum = 0;
+        int total = 0;
+        for(int num : nums){
+            total = (total + num)%p;
         }
-        if (totalSum == 0) return 0;  // If the total sum is divisible by p, no subarray needs to be removed.
 
-        int target = totalSum;
-        HashMap<Integer, Integer> modMap = new HashMap<>();
-        modMap.put(0, -1);  // Initialize with mod 0 at index -1 for edge cases.
-        int prefixSum = 0, minLength = nums.length;
-
-        for (int i = 0; i < nums.length; i++) {
-            prefixSum = (prefixSum + nums[i]) % p;
-            int neededMod = (prefixSum - target + p) % p;  // Calculate the mod we need to find in the map.
-
-            if (modMap.containsKey(neededMod)) {
-                minLength = Math.min(minLength, i - modMap.get(neededMod));
+        if(total == 0) return 0;
+        for(int i = 0; i<nums.length; i++){
+            sum = (sum+nums[i])%p;
+            int check = (sum - total + p)%p;
+            if(map.containsKey(check)){
+                ans = Math.min(ans, i-map.get(check));
             }
-            modMap.put(prefixSum, i);  // Update the map with the current prefix sum.
+            map.put(sum, i);
         }
 
-        return minLength == nums.length ? -1 : minLength;  // If no valid subarray was found, return -1.
+        return ans == nums.length? -1 : ans;
     }
 }
