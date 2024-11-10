1529840deba4cd7c2ccc062b6335fc041d01c05a
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-11-10 19:45:45 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 9.33%] - LeetPush

--- Binary Tree/144-Binary-Tree-Preorder-Traversal.java
+++ Binary Tree/144-Binary-Tree-Preorder-Traversal.java
@@ -16,14 +16,15 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
-        func(root , ans);
-        return ans;
-    }
-    private void func(TreeNode currNode , List<Integer> ans){
-        if(currNode == null)return;
-
-        ans.add(currNode.val);
-        func(currNode.left , ans);
-        func(currNode.right , ans);
+		if(root == null) return ans;
+		Stack<TreeNode> st = new Stack<TreeNode>();
+		st.push(root);
+		while(!st.isEmpty()){
+			root = st.pop();
+			ans.add(root.val);
+			if(root.right != null)st.push(root.right);
+			if(root.left != null)st.push(root.left);
+		}
+		return ans;
     }
 }
