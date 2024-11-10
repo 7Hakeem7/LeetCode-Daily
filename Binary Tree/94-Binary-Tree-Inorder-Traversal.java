0d6f68f11d77ea5084d29b19894e9c5d6a4a1d7f
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-11-10 19:15:40 +0530

    [94] [Time Beats: 100.00%] [Memory Beats: 81.54%] - LeetPush

---
+++ Binary Tree/94-Binary-Tree-Inorder-Traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        func(root , ans);
+        return ans;
+    }
+    private void func(TreeNode currNode , List<Integer> ans){
+        if(currNode == null)return;
+
+        func(currNode.left , ans);
+        ans.add(currNode.val);
+        func(currNode.right , ans);
+    }
+}
