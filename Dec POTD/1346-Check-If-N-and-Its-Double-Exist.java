a45255ee1b4f9b8d3c4935bce292257b40b7565f
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-12-01 14:22:37 +0530

    [1346] [Time Beats: 93.52%] [Memory Beats: 26.42%] - LeetPush

--- Dec POTD/1346-Check-If-N-and-Its-Double-Exist.java
+++ Dec POTD/1346-Check-If-N-and-Its-Double-Exist.java
@@ -1,17 +1,11 @@
+import java.util.HashSet;
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        //so the thing is we stand on one element
-        //and check if there exists a 2 x the curr element in the array?
-        //We can use a HashMap for that.
-        //also we may not use it twice since i != j (0 case)
-        HashMap<Integer,Integer> hm = new HashMap<>();
-
-        for(int i = 0; i < arr.length; i++) {
-            if(hm.containsValue(arr[i]*2) || hm.containsKey(arr[i])) {
-                return true;
-            }
-            hm.put(arr[i]*2,arr[i]);
-            
+        HashSet<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i * 2)) return true;
+            if (i % 2 == 0 && set.contains(i / 2)) return true;
+            set.add(i);
         }
         return false;
     }
