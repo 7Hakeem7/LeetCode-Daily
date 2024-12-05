c7726c28af43527372118b391c45d8c2abe28410
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-12-05 16:05:07 +0530

    [2337] [Time Beats: 100.00%] [Memory Beats: 54.17%] - LeetPush

--- Dec POTD/2337-Move-Pieces-to-Obtain-a-String.java
+++ Dec POTD/2337-Move-Pieces-to-Obtain-a-String.java
@@ -1,31 +1,25 @@
 class Solution {
-    public boolean canChange(String st, String tar) {
-        //two pointers -> keep left and right
-        //move the right on the first piece of target6 from left to right, get the the position of it;
-        //move the left till it matches the char at right
-        int n = tar.length();
-        int i=0,j=0;
+    public boolean canChange(String start, String target) {
+        char[] s = start.toCharArray();
+        char[] t = target.toCharArray();
+        int n = s.length;
+        int i=0, j=0;
+
         while(i<=n && j<=n){
-            
-            while(i<n && tar.charAt(i)=='_') i++;
-            while(j<n && st.charAt(j)=='_') j++;
-            
-            if(i==n || j==n){
-                return i==n && j==n;
-            }
-            
-            if(tar.charAt(i)!=st.charAt(j)) return false;
-            
-            if(tar.charAt(i)=='L'){
-                if(j<i) return false;
-            }
-            else{
-                if(i<j) return false;
-            }
-            
+
+            while(i<n && s[i] == '_') i++;
+            while(j<n && t[j] == '_') j++;
+
+            if(i == n || j==n) return i==n && j==n;
+
+            if(s[i]!=t[j]) return false;
+            if(s[i] == 'L' && i<j) return false;
+            else if(s[i] == 'R' && i>j) return false;
+
             i++;
             j++;
         }
+
         return true;
     }
 }
