5a1bf7311f50f7de1f7356ecfd9e7c68e9b1e1c5
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-12-07 00:26:59 +0530

    [2554] [Time Beats: 19.02%] [Memory Beats: 46.36%] - LeetPush

--- Dec POTD/2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.java
+++ Dec POTD/2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.java
@@ -1,34 +1,33 @@
-// class Solution {
-//     public int maxCount(int[] banned, int n, int maxSum) {
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
         //the only thing you need to do is
         //choose the numbers that are not in arr[], between 1 - n , totalSum <= maxSum;
         //take a HashMap of banned numbers
-        //take an arraylist of size n with values 1 to n
-        //traverse list and check if the currVal exists in Map
-        //if not then add it to total sum and add it to the Map and count++
-        //do search = abs.(totalSum - maxSum) in the list
-        //
-//     }
-// }
-
-class Solution {
-    public int maxCount(int[] banned, int n, int maxSum) {
-        int[] arr=new int[10001];
-        for(int i=0; i<banned.length; i++){
-            arr[banned[i]]=1;
+        //traverse values 1 - n and check if the currVal exists in Map
+        //if not then add it to total sum and count++
+        ArrayList<Integer>list=new ArrayList<>();
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<banned.length;i++){
+         map.put(banned[i],1);
         }
-
-        long sum=0;
-        int cnt=0;
-        for(int i=1; i<=n; i++){
-            if(arr[i]==1){continue;}
-            sum+=i;
-            if(sum>maxSum){
-               break;
+        int i=1;
+        while(i<=n){
+            if(!map.containsKey(i)){
+                list.add(i);
             }
-            cnt++;
+            i++;
         }
-
-        return cnt;
+        int count=0;
+        int sum=0;
+        for( i=0;i<list.size();i++){
+            sum+=list.get(i);
+            if(sum>maxSum){
+                break;
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
     }
 }
