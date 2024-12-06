f946a924831c90b819a50a9d85ddda9b01ff524f
7Hakeem7 <111656805+7Hakeem7@users.noreply.github.com>
2024-12-07 00:34:23 +0530

    [2554] [Time Beats: 77.72%] [Memory Beats: 83.64%] - LeetPush

--- Dec POTD/2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.java
+++ Dec POTD/2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.java
@@ -5,27 +5,29 @@ class Solution {
         //take a HashMap of banned numbers
         //traverse values 1 - n and check if the currVal exists in Map
         //if not then add it to total sum and count++
-        ArrayList<Integer>list=new ArrayList<>();
+        // ArrayList<Integer>list=new ArrayList<>();
         HashMap<Integer,Integer>map=new HashMap<>();
         for(int i=0;i<banned.length;i++){
          map.put(banned[i],1);
         }
-        int i=1;
-        while(i<=n){
-            if(!map.containsKey(i)){
-                list.add(i);
-            }
-            i++;
-        }
+        // int i=1;
+        // while(i<=n){
+        //     if(!map.containsKey(i)){
+        //         list.add(i);
+        //     }
+        //     i++;
+        // }
         int count=0;
         int sum=0;
-        for( i=0;i<list.size();i++){
-            sum+=list.get(i);
-            if(sum>maxSum){
-                break;
-            }
-            else{
-                count++;
+        for(int i=1;i<=n;i++){
+            if(!map.containsKey(i)){
+                sum += i;
+                if(sum>maxSum){
+                    break;
+                }
+                else{
+                    count++;
+                }
             }
         }
         return count;
