f402275048aec20b33c141e5635b9dd8b8053bc5
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2024-12-16 22:19:09 +0530

    [3264] [Time Beats: 33.80%] [Memory Beats: 89.91%] - LeetPush

--- Dec POTD/3264-Final-Array-State-After-K-Multiplication-Operations-I.java
+++ Dec POTD/3264-Final-Array-State-After-K-Multiplication-Operations-I.java
@@ -1,39 +1,37 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        //take a priority queue  minHeap
-        //get the top and x multiplier and k --;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair a , Pair b) ->{
-            if(a.value != b.value) return (a.value - b.value);
+        // Priority queue for min heap, sorts by value and then by index
+        PriorityQueue<Pair> pq = new PriorityQueue<>((Pair a, Pair b) -> {
+            if (a.value != b.value) return (a.value - b.value);
             else return (a.index - b.index);
         });
 
-        for(int i = 0; i < nums.length; i++){
-            pq.offer(new Pair(nums[i] , i));
+        // Add all elements to the priority queue
+        for (int i = 0; i < nums.length; i++) {
+            pq.offer(new Pair(nums[i], i));
         }
 
-        while(k-- > 0){
-            int currMin = pq.peek().value;
-            int currIndex = pq.peek().index;
-            int x = currMin * multiplier;
-            pq.poll();
-            pq.add(new Pair(x , currIndex));
+        // Perform k operations
+        while (k-- > 0) {
+            // Get the minimum element
+            Pair minPair = pq.poll();
+
+            // Update the value in nums
+            nums[minPair.index] *= multiplier;
+
+            // Add the updated value back into the priority queue
+            pq.offer(new Pair(nums[minPair.index], minPair.index));
         }
 
-        while(!pq.isEmpty()){
-            int currVal = pq.peek().value;
-            int atPosition = pq.peek().index;
-            nums[atPosition] = currVal;
-            pq.poll();
-        }
         return nums;
     }
 }
 
-class Pair{
+class Pair {
     int value;
     int index;
 
-    Pair(int value, int index){
+    Pair(int value, int index) {
         this.value = value;
         this.index = index;
     }
