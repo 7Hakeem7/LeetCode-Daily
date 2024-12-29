ae538643bb076a363fbad3ad6e9490c6b0059972
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2024-12-29 20:36:41 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 37.75%] - LeetPush

--- Recursion/50-Pow(x,-n).java
+++ Recursion/50-Pow(x,-n).java
@@ -1,19 +1,30 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == 0) return 1;
-        long num = n;
-        if (num < 0) {
-            x = 1 / x;
-            num = -num;
-        }
-        double result = 1;
-        while (num > 0) {
-            if (num % 2 == 1) {
-                result *= x;
+        //if n is even do n/2 and x * x
+        //if n is odd do x * x^n-1;
+        //base cases:
+        if (n < 0) {
+            // Special handling for the case when n is Integer.MIN_VALUE
+            if (n == Integer.MIN_VALUE) {
+                return 1.0 / func(x, Integer.MAX_VALUE) / x;  // Handle overflow by using Integer.MAX_VALUE
             }
-            x *= x;  // Square the base
-            num /= 2; // Divide the exponent by 2
+            return 1.0 / func(x, -n);  // Calculate power for positive n
         }
-        return result;
+        return func(x, n);
+        // return ans;
+    }
+    private double func(double x , int n){
+        double val = 1;
+        if(n == 0){
+            return 1.0;
+        }
+        if(n % 2 == 1){
+            val = x * func(x , n - 1);
+        }
+        else{
+            n = n / 2;
+            val = func(x * x , n);
+        }
+        return val;
     }
 }
