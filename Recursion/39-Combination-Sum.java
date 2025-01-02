ffbeca28bede47aaedb0002eddbc9b6876632b0c
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2025-01-02 12:36:19 +0530

    [39] [Time Beats: 83.56%] [Memory Beats: 52.67%] - LeetPush

--- Recursion/39-Combination-Sum.java
+++ Recursion/39-Combination-Sum.java
@@ -1,42 +1,27 @@
 class Solution {
-    public void func(List<Integer> v, int i, int sum, List<Integer> v2, List<List<Integer>> ans) {
-        // Base case: if the sum is zero, add the current subsequence to the result
-        if (sum == 0) {
-            ans.add(new ArrayList<>(v2));
-            return;
-        }
-        
-        // Base case: if the sum becomes negative or no elements are left
-        if (sum < 0 || i < 0) {
-            return;
-        }
-
-        // Exclude the current element and move to the next
-        func(v, i - 1, sum, v2, ans);
-        
-        // Include the current element in the subsequence
-        v2.add(v.get(i));
-        
-        // Recursively call the function with the included element
-        func(v, i, sum - v.get(i), v2, ans);
-        
-        // Backtrack by removing the last added element
-        v2.remove(v2.size() - 1);
-    }
-
-    // Main function to find all unique combinations of candidates that sum to the target
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> v = new ArrayList<>();
-        
-        // Convert the array to a list for easier manipulation
-        for (int num : candidates) {
-            v.add(num);
-        }
-        
-        // Start the recursive process
-        func(v, v.size() - 1, target, new ArrayList<>(), ans);
-        
+        List<Integer> list = new ArrayList<>();
+        func(0 , candidates , target , list , ans);
         return ans;
+    }
+    private void func(int ind , int[] candidates , int k , List<Integer> list , List<List<Integer>> ans){
+        //base cases:
+        // if(ind == candidates.length && k == 0){
+        //     ans.add(new ArrayList<>(list));
+        //     return;
+        // }
+        if(k < 0 || ind == candidates.length){
+            return;
+        }
+        if(k == 0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        list.add(candidates[ind]);
+        func(ind , candidates , k - candidates[ind], list, ans);
+        list.remove(list.size() - 1);
+
+        func(ind + 1 , candidates, k, list,ans);
     }
 }
