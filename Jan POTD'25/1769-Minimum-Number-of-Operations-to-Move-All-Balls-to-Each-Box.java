c19ba31ddaf5ac8f3eb1d08cc98bb3129e9b1118
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2025-01-06 18:48:08 +0530

    [1769] [Time Beats: 94.79%] [Memory Beats: 32.47%] - LeetPush

--- Jan POTD'25/1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.java
+++ Jan POTD'25/1769-Minimum-Number-of-Operations-to-Move-All-Balls-to-Each-Box.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int len = boxes.length();
-        int[] ans = new int[len];
-        for(int i=0;i<len;i++){
-            int cost = 0;
-            for(int j=0;j<len;j++){
-                if(boxes.charAt(j)=='1') cost += Math.abs(i-j);
-            }
-            ans[i] = cost;
+        int[] forward = new int[len];
+        int prev = (boxes.charAt(0)=='0'?0:1);
+        for(int i=1;i<len;i++){
+            forward[i] += forward[i-1]+prev;
+            prev += (boxes.charAt(i)=='0'?0:1);
         }
-        return ans;
+        prev = (boxes.charAt(len-1)=='0'?0:1);
+        int[] back = new int[len];
+        for(int i=len-2;i>=0;i--){
+            back[i] += back[i+1]+prev;
+            prev += (boxes.charAt(i)=='0'?0:1);
+        }
+        int[] op = new int[len];
+        for(int i=0;i<len;i++) op[i] = forward[i]+back[i];
+        return op;
     }
 }
