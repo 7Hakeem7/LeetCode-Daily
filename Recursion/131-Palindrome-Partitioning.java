e3843d846d60951ecd45412fd64c7ddc84771705
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2025-01-07 22:08:59 +0530

    [131] [Time Beats: 98.11%] [Memory Beats: 52.61%] - LeetPush

--- Recursion/131-Palindrome-Partitioning.java
+++ Recursion/131-Palindrome-Partitioning.java
@@ -2,31 +2,30 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
         List<String> list = new ArrayList<>();
-        generate(s, 0 , ans , list);
+        func(0, s, ans, list);
         return ans;
     }
-    private void generate(String s , int ind , List<List<String>> ans , List<String> list){
-        int n = s.length();
-        if(ind == n){
+    private void func(int ind, String s, List<List<String>> ans, List<String> list){
+        //base case:
+        //if it reaches end means we will be adding it into our final list
+        //we are sure that without check if the current is a palidrom we wont move further so when we reach the end it will be a valid one only
+        if(ind == s.length()){
             ans.add(new ArrayList<>(list));
             return;
         }
-        for (int j = ind ; j <= n-1 ; j++){
-            if(isPalindrome(s, ind , j)){
-                String substr = s.substring(ind , j + 1);
-                list.add(substr);
-                generate(s , j + 1 , ans , list);
+        for(int i = ind; i < s.length(); i++){
+            if(isPalindrome(ind, i, s)){
+                list.add(s.substring(ind , i + 1));
+                func(i + 1, s, ans, list);
                 list.remove(list.size() - 1);
             }
         }
     }
-    private boolean isPalindrome(String s , int left , int right){
-        while(left <= right){
-            if(s.charAt(left) != s.charAt(right)){
+    private boolean isPalindrome(int start , int end , String s){
+        while(start <= end){
+            if(s.charAt(start++) != s.charAt(end--)){
                 return false;
             }
-            left ++;
-            right --;
         }
         return true;
     }
