c99f699dc32758355bf95c84481dd5a2f0c6342e
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2025-01-15 23:52:26 +0530

    [912] [Time Beats: 74.12%] [Memory Beats: 51.67%] - LeetPush

--- Heaps/912-Sort-an-Array.java
+++ Heaps/912-Sort-an-Array.java
@@ -1,13 +1,48 @@
 class Solution {
+    int[] nums;
+    int[] tmp;
     public int[] sortArray(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a , b) -> a - b);
-        for(int num : nums){
-            pq.add(num);
+        this.nums = nums;
+        if (nums == null || nums.length == 0) {
+            return nums;
         }
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = pq.poll();
-        }
+        this.tmp = new int[nums.length];
+        mergeSort(0, nums.length-1);
         return nums;
+    }
 
+    void mergeSort(int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(start, mid);
+            mergeSort(mid+1, end);
+            merge(start, mid, end);
+        }
+    }
+
+    void merge(int start, int mid, int end) {
+        int i = start;
+        int j = mid+1;
+        int index = 0;
+
+        while (i <= mid && j <= end) {
+            if (nums[i] < nums[j]) {
+                tmp[index++] = nums[i++];
+            } else {
+                tmp[index++] = nums[j++];
+            }
+        }
+
+        while (j <= end) {
+            tmp[index++] = nums[j++];
+        }
+
+        while (i <= mid) {
+            tmp[index++] = nums[i++];
+        }
+
+        for (int w = 0;w < index; w++) {
+            nums[w + start] = tmp[w];
+        }
     }
 }
