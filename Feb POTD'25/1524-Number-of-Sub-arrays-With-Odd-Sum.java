47b4145479056f08843d52a2c2e506050bad7402
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2025-02-26 00:22:09 +0530

    [1524] [Time Beats: 83.08%] [Memory Beats: 43.82%] - LeetPush

--- Feb POTD'25/1524-Number-of-Sub-arrays-With-Odd-Sum.java
+++ Feb POTD'25/1524-Number-of-Sub-arrays-With-Odd-Sum.java
@@ -1,11 +1,19 @@
 class Solution {
     public int numOfSubarrays(int[] arr) {
-        long oddCount = 0, prefixSum = 0;
-        for (int a : arr) {
-            prefixSum += a;
-            oddCount += prefixSum % 2;
+        int MOD=1_000_000_007;
+        int odd=0,even=1;
+        int sum=0,result=0;
+        for (int num : arr) {
+            sum += num;
+            if (sum % 2 == 0) {
+                result = (result + odd) % MOD;
+                even++;
+            } else {
+                result = (result + even) % MOD;
+                odd++;
+            }
         }
-        oddCount += (arr.length - oddCount) * oddCount;
-        return (int)(oddCount % 1_000_000_007);       
+        
+        return result;
     }
 }
