ca3c57fbcb35fc882bcc985fcb27eefabf2da398
Hakeem <111656805+7Hakeem7@users.noreply.github.com>
2025-03-17 15:42:14 +0530

    [2206] [Time Beats: 100.00%] [Memory Beats: 71.91%] - LeetPush

--- March POTD'25/Divide-Array-Into-Equal-Pairs.java
+++ March POTD'25/Divide-Array-Into-Equal-Pairs.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-        //sort the array
-        //goal = nums.length / 2
-        //if no of pairs == goal return true elese false
-        Arrays.sort(nums);
-        int goal = nums.length / 2;
-        int count = 0;
-        //2 2 2 2 3 3
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] == nums[i - 1]){
-                count++;
-                i++;
-            }
-            if(count == goal)return true;
+        int[] freq = new int[501];
+
+        for(int i = 0; i<nums.length; i++){
+            freq[nums[i]]++;
         }
-        return false;
+
+        for(int i = 0; i<501; i++){
+            if(freq[i]%2 != 0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
